6a613c0fd3c8aea7eadd8208be3501922401dc70
MyBooty165 <126267638+MyBooty165@users.noreply.github.com>
2023-05-22 19:32:50 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -9,5 +9,11 @@ javascript:(function() {
     div.style.backgroundImage = 'url("https://steamuserimages-a.akamaihd.net/ugc/2009206964543793942/1CA1014D0B5A40E51608DE058DE4638FF42EAF9D/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true")';
     div.style.backgroundSize = 'cover';
     div.style.backgroundPosition = 'center';
+    div.style.display = 'flex';
+    div.style.justifyContent = 'center';
+    div.style.alignItems = 'center';
+    div.style.color = 'darkgreen';
+    div.style.fontSize = '5em';
+    div.innerText = "Successfully Imported Ambasing Virus";
     document.body.appendChild(div);
 })();
